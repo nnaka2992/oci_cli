572c269b02b15fc1779e1a158479928d0ce2b133
nnaka2992 <nnaka2992@gmail.com>
2021-10-09 01:06:38 +0900

    remove debug message

--- oci.bat
+++ oci.bat
@@ -7,7 +7,7 @@ if not defined OCI_CONFIG_DIR (
     echo Use !OCI_CONFIG_DIR! instead.
     echo;
 )
-if exist %OCI_CONFIG_DIR%\nul echo exist mkdir %OCI_CONFIG_DIR%
+if exist %OCI_CONFIG_DIR%\nul mkdir %OCI_CONFIG_DIR%
 
 docker run -it ^
        -v %OCI_CONFIG_DIR%:/root/.oci ^
