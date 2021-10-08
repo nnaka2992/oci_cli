f9dff534abd9c21b37f79c01f8916736426664e6
nnaka2992 <nnaka2992@gmail.com>
2021-10-08 22:26:42 +0900

    support file path with space

--- oci.sh
+++ oci.sh
@@ -5,15 +5,14 @@ if [[ -z "$OCI_CONFIG_DIR" ]]; then
     current=`pwd`
     cd $(dirname $0)
     OCI_CONFIG_DIR=`pwd`/oci
-    mkdir -p $OCI_CONFIG_DIR
-    cd $current
+    cd "$current"
     printf "OCI_CONFIG_DIR is unset\n Use $OCI_CONFIG_DIR instead.\n"
 fi
 
+mkdir -p "$OCI_CONFIG_DIR"
+
 docker run -it \
-       -v $OCI_CONFIG_DIR:/root/.oci \
+       -v "$OCI_CONFIG_DIR":/root/.oci \
        -e OCI_CONFIG=/root/.oci/config \
        -p 127.0.0.1:8181:8181 \
        nnaka2992/oci_cli:latest $@
-
-echo
